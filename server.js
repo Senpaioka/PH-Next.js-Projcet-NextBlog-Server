7a339dba7720569899c4e2d0a9b9284a1f31a7b2
senpaioka@linux <abdulmannan7589@gmail.com>
2025-11-24 23:54:06 +0600

    blogging system added

--- server.js
+++ server.js
@@ -41,6 +41,15 @@ const firebaseVerificationToken = async (req, res, next) => {
     const decode = await admin.auth().verifyIdToken(token);
     req.user = decode;
 
+    const fullUser = await admin.auth().getUser(decode.uid);
+
+    req.user = {
+      uid: decode.uid,
+      email: fullUser.providerData[0]?.email || decode.email,
+      displayName: fullUser.displayName,
+      providerId: fullUser.providerData[0]?.providerId || null,
+    };
+
     // continue
     next();
 
@@ -91,6 +100,7 @@ client.connect()
 // database setup
 const database = client.db('next-blog');
 const user_collection = database.collection('users');
+const blog_collection = database.collection('blogs');
 
 
   /* API Calls */
@@ -153,6 +163,65 @@ app.post("/registration",firebaseVerificationToken, async(req, res) => {
 });
 
 
+
+// create blog
+app.post("/create-blog", firebaseVerificationToken, async(req, res) => {
+
+  const user = req.user;
+  const get_data = req.body;
+
+  const blog_data = {
+    ...get_data,
+    displayName: user.displayName,
+    userId: user.email,
+    user_uid: user.uid || null,
+    created_at: new Date(),
+    updated_at: new Date(),
+  }
+
+  try {
+    await blog_collection.insertOne(blog_data);
+    res.status(201).json({message: "Blog Published"});
+  }catch(error){
+    res.status(400).json({message: "Blog Publish Unsuccessful."});
+  }
+})
+
+
+
+// get all blogs
+app.get('/get-blogs', async(req, res) => {
+
+  const result = await blog_collection.find({}, {projection: {content: 0, user_uid: 0, userId: 0} })
+  .sort({ updated_at: -1 }) 
+  .toArray();
+  res.send(result);
+
+})
+
+
+// get blog details
+app.get('/blog-details/:id', firebaseVerificationToken, async(req, res) => {
+    try {
+      const blog_id = req.params.id;
+      const params = { _id: new ObjectId(blog_id) };
+      const blog = await blog_collection.findOne(params);
+
+      // user
+      const email = blog.userId;
+      const user = await user_collection.findOne({ email: email });
+
+      // combined both result
+      const result = {
+        ...blog,
+        userInfo: user || null,
+      };
+
+      res.send(result);
+    }catch(error){
+     res.status(404).json({message: "Blog Not Found."}); 
+    }
+})
 
 
 
